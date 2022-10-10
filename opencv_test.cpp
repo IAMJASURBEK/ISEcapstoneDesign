48cabb0f4426b1f782ff75e5dc21cc3ad51fc59b
IAMJASURBEK <90182787+IAMJASURBEK@users.noreply.github.com>
2022-10-10 23:35:31 +0900

    Add files via upload

---
+++ opencv_test.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/core/core.hpp> 
+#include <opencv2/imgcodecs.hpp> 
+#include <opencv2/highgui/highgui.hpp> 
+#include <iostream> 
+#include <string> 
+using namespace cv; 
+using namespace std;
+int main()
+{
+Mat image = imread("image.jpg"); // Read the file 
+namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display. 
+imshow("Display window", image); // Show our image inside it. 
+waitKey(0); // Wait for a keystroke in the window 
+return 0;
+}
+
+// Execution command:
+// g++ opencv_test.cpp -o opencv_test -I/usr/local/include/opencv4 -I/usr/local/include -L/usr/local/lib -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_core
+
